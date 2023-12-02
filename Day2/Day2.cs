4a2e746eadb5100d55b96f6694708de65bf356e5
DazzWh <darrellwhittall@gmail.com>
2023-12-02 12:33:54 +0000

    Day2 part 2

--- Day2/Day2.cs
+++ Day2/Day2.cs
@@ -8,35 +8,22 @@ public static class Day2
     public static void Run()
     {
         // Limit = 12 red, 13 green, 14 blue.
-        var lines = File.ReadAllLines("Day2Input.txt").ToList();
-
-        // Split into games
-        lines = lines.Select(l => l.Split(':')[1]).ToList();
-
-        var shown = lines.Select(l => l.Split(';'));
-        var maxInEachGame = shown.Select(ParseGame).ToList();
-
-
-        var total = 0;
-        foreach (var (r, i) in maxInEachGame.Select((item, index) => (item, index)))
-        {
-            if (r.R <= 12 && r.G <= 13 && r.B <= 14)
-            {
-                total += i + 1;
-            }
-        }
-        
-        Console.WriteLine(total);
+        Console.WriteLine(
+            File.ReadAllLines("Day2Input.txt")
+            .Select(l => l.Split(':')[1])
+            .Select(l => l.Split(';'))
+            .Select(ParseGame)
+            .Select(r => r.R * r.G * r.B)
+            .Sum());
     }
 
-    private static (int R, int G, int B) ParseGame(string[] shows)
+    private static (int R, int G, int B) ParseGame(string[] game)
     {
         int r = 0, g = 0, b = 0;
 
-        foreach (var show in shows)
+        foreach (var show in game)
         {
-            var numbers = show.Split(',');
-            foreach (var num in numbers)
+            foreach (var num in show.Split(','))
             {
                 var s = num.Trim().Split(' ');
                 switch (s[1])
