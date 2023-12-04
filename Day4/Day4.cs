63cd8e0a9911d70502343ddc94d43855981fad66
DazzWh <darrellwhittall@gmail.com>
2023-12-04 09:22:58 +0000

    Day 4 Part 1

---
+++ Day4/Day4.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day4;
+
+public static class Day4
+{
+    public static void Run()
+    {
+        Console.WriteLine(
+            File.ReadAllLines("Day4.txt")
+                .Select(l => new Regex(@"(Card\s*\d*:)(.*)\|(.*)").Matches(l)[0].Groups)
+                .Select(gc =>
+                    (gc[2].Value.Split(' ', StringSplitOptions.RemoveEmptyEntries),
+                     gc[3].Value.Split(' ', StringSplitOptions.RemoveEmptyEntries)))
+                .Aggregate(0, (score, cards) =>
+                    score + Math.Max(0, 1 <<  cards.Item1.Intersect(cards.Item2).Count() - 1)));
+    }
+}
