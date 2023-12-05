6ebcc26a2e373c9ba86e9128c2c16f4d00af930e
DazzWh <darrellwhittall@gmail.com>
2023-12-05 11:12:51 +0000

    D5P1

---
+++ Day5/LongExtenstions.cs
@@ -0,0 +1,9 @@
+﻿namespace Day5;
+
+public static class LongExtenstions
+{
+    public static bool IsBetween(this long num, long start, long end)
+    {
+        return num >= start && num <= end;
+    }
+}
---
+++ Day5/Program.cs
@@ -0,0 +1,17 @@
+﻿new Day5.Day5().Run();
+
+public class AlmanacMap(string source, string destination)
+{
+    public string Source = source;
+    public string Destination = destination;
+    public List<AlmanacRange> Ranges = new();
+}
+
+public class AlmanacRange(long destinationStart, long sourceStart, long range)
+{
+    public long DestinationStart = destinationStart;
+    public long SourceStart = sourceStart;
+    public long Range = range;
+}
+
+
