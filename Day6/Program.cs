9e3380b97f4e93c6914e739f66cbabb40ea498d4
DazzWh <darrellwhittall@gmail.com>
2023-12-06 08:30:07 +0000

    D6P2

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -2,16 +2,18 @@
 var times =
     input[0].Split(':')[1]
         .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-        .Select(int.Parse)
+        .Select(long.Parse)
         .ToArray();
 
 var dists =
     input[1].Split(':')[1]
         .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-        .Select(int.Parse)
+        .Select(long.Parse)
         .ToArray();
 
-var answer = new int[times.Length];
+long answer = 1;
+
+var lockObj = new object();
 
 for (var i = 0; i < times.Length; i++)
 {
@@ -19,17 +21,20 @@ for (var i = 0; i < times.Length; i++)
     var recordDist = dists[i];
     var waysToBeat = 0;
 
-    for (var timeHeld = 0; timeHeld < maxTime; timeHeld++)
+    Parallel.For(0, maxTime, timeHeld =>
     {
         var distanceTraveled = timeHeld * (maxTime - timeHeld);
-        
+
         if (distanceTraveled > recordDist)
         {
-            waysToBeat++;
+            lock (lockObj)
+            {
+                waysToBeat++;
+            }
         }
-    }
+    });
 
-    answer[i] = waysToBeat;
+    answer *= waysToBeat;
 }
 
-Console.WriteLine(answer.Aggregate((x, y) => x * y));
+Console.WriteLine(answer);
