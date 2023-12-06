2a3059bf7838b96a15de34d52e4d156b414f6dcc
DazzWh <darrellwhittall@gmail.com>
2023-12-06 08:21:27 +0000

    D6P1

---
+++ Day6/Program.cs
@@ -0,0 +1,35 @@
+﻿var input = File.ReadAllLines("Input.txt").ToList();
+var times =
+    input[0].Split(':')[1]
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+var dists =
+    input[1].Split(':')[1]
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+var answer = new int[times.Length];
+
+for (var i = 0; i < times.Length; i++)
+{
+    var maxTime = times[i];
+    var recordDist = dists[i];
+    var waysToBeat = 0;
+
+    for (var timeHeld = 0; timeHeld < maxTime; timeHeld++)
+    {
+        var distanceTraveled = timeHeld * (maxTime - timeHeld);
+        
+        if (distanceTraveled > recordDist)
+        {
+            waysToBeat++;
+        }
+    }
+
+    answer[i] = waysToBeat;
+}
+
+Console.WriteLine(answer.Aggregate((x, y) => x * y));
