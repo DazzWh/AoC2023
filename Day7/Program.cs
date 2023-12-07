06c714adbefa2f44b864e7e0cbfe999247e89b89
DazzWh <darrellwhittall@gmail.com>
2023-12-07 19:30:14 +0000

    D7P1

---
+++ Day7/Program.cs
@@ -0,0 +1,51 @@
+﻿var cardOrder = new List<char> { 'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2' };
+
+Console.WriteLine(File.ReadAllLines("input.txt")
+    .Select(l =>
+        new
+        {
+            cards = l.Split(' ')[0],
+            bid = int.Parse(l.Split(' ')[1]),
+            strength = calculateHandStrength(l.Split(' ')[0])
+        })
+    .OrderBy(h => h.strength)
+    .ThenByDescending(h => cardOrder.IndexOf(h.cards[0]))
+    .ThenByDescending(h => cardOrder.IndexOf(h.cards[1]))
+    .ThenByDescending(h => cardOrder.IndexOf(h.cards[2]))
+    .ThenByDescending(h => cardOrder.IndexOf(h.cards[3]))
+    .ThenByDescending(h => cardOrder.IndexOf(h.cards[4]))
+    .Select((h, i) => h.bid * (i + 1))
+    .Sum());
+
+return;
+
+int calculateHandStrength(string cards)
+{
+    var uniqueCardValues = cards.Distinct().Count();
+
+    var cardCounts = cards
+        .GroupBy(c => c)
+        .Select(g => new { Card = g.Key, Count = g.Count() })
+        .ToArray();
+
+    return cards switch
+    {
+        var fiveOfAKind when uniqueCardValues == 1 => 6,
+
+        var fourOfAKind when uniqueCardValues == 2 &&
+                             (cardCounts.First().Count, cardCounts.Last().Count) is (1, 4) or (4, 1) => 5,
+
+        var fullHouse when uniqueCardValues == 2 &&
+                           (cardCounts.First().Count, cardCounts.Last().Count) is (3, 2) or (2, 3) => 4,
+
+        var threeOfAKind when cardCounts.Any(c => c.Count == 3) => 3,
+
+        var twoPair when cardCounts.Count(c => c.Count == 2) == 2 => 2,
+
+        var onePair when cardCounts.Count(c => c.Count == 2) == 1 => 1,
+
+        var highCard when uniqueCardValues == 5 => 0,
+
+        _ => throw new ArgumentOutOfRangeException(nameof(cards), cards, null)
+    };
+}
