6deea91b52cefe125ba349c1620b0924d19fcf74
DazzWh <darrellwhittall@gmail.com>
2023-12-07 19:39:28 +0000

    D7P2

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,26 +1,37 @@
-﻿var cardOrder = new List<char> { 'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2' };
+﻿using MoreLinq.Extensions;
 
-Console.WriteLine(File.ReadAllLines("input.txt")
-    .Select(l =>
-        new
-        {
-            cards = l.Split(' ')[0],
-            bid = int.Parse(l.Split(' ')[1]),
-            strength = calculateHandStrength(l.Split(' ')[0])
-        })
-    .OrderBy(h => h.strength)
-    .ThenByDescending(h => cardOrder.IndexOf(h.cards[0]))
-    .ThenByDescending(h => cardOrder.IndexOf(h.cards[1]))
-    .ThenByDescending(h => cardOrder.IndexOf(h.cards[2]))
-    .ThenByDescending(h => cardOrder.IndexOf(h.cards[3]))
-    .ThenByDescending(h => cardOrder.IndexOf(h.cards[4]))
-    .Select((h, i) => h.bid * (i + 1))
-    .Sum());
+var cardOrder = new List<char> { 'A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J' };
+
+Console.WriteLine(
+    File.ReadAllLines("input.txt")
+        .Select(l =>
+            new
+            {
+                cards = l.Split(' ')[0],
+                bid = int.Parse(l.Split(' ')[1]),
+                strength = calculateHandStrength(l.Split(' ')[0])
+            })
+        .OrderBy(h => h.strength)
+        .ThenByDescending(h => cardOrder.IndexOf(h.cards[0]))
+        .ThenByDescending(h => cardOrder.IndexOf(h.cards[1]))
+        .ThenByDescending(h => cardOrder.IndexOf(h.cards[2]))
+        .ThenByDescending(h => cardOrder.IndexOf(h.cards[3]))
+        .ThenByDescending(h => cardOrder.IndexOf(h.cards[4]))
+        .Select((h, i) => h.bid * (i + 1))
+        .Sum()
+);
 
 return;
 
 int calculateHandStrength(string cards)
 {
+    if (cards.Contains('J'))
+    {
+        return cardOrder[..^1]
+            .Select(possibleCard => cards.Replace('J', possibleCard))
+            .Max(calculateHandStrength);
+    }
+
     var uniqueCardValues = cards.Distinct().Count();
 
     var cardCounts = cards
