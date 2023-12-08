01b6ed27f227244a5bed6b6d3254e4dc4537c60a
DazzWh <darrellwhittall@gmail.com>
2023-12-08 09:16:36 +0000

    D8P2

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 var input = File.ReadAllLines("Input.txt").ToList();
 
@@ -11,15 +10,46 @@ var nodes = input
     .Select(gc => new { Pos = gc[1].Value, L = gc[2].Value, R = gc[3].Value })
     .ToArray();
 
-var currentNode = nodes.First(n => n.Pos == "AAA");
+var currentNodes = nodes.Where(n => n.Pos.Last() == 'A').ToArray();
 
-var steps = 0;
-while (currentNode.Pos != "ZZZ")
+// Get smallest steps for each node
+var smallestSteps = new List<long>();
+
+foreach (var node in currentNodes)
 {
-    var nextLocation = pathToFollow[steps % pathToFollow.Length] == 'R' ? currentNode.R : currentNode.L;
-    steps++;
+    var currentNode = node;
+    long steps = 0;
 
-    currentNode = nodes.First(n => n.Pos == nextLocation);
+    while (currentNode.Pos.Last() != 'Z')
+    {
+        var nextLocation = pathToFollow[(int)steps % pathToFollow.Length] == 'R' ? currentNode.R : currentNode.L;
+        steps++;
+
+        currentNode = nodes.First(n => n.Pos == nextLocation);
+    }
+    
+    smallestSteps.Add(steps);
 }
 
-Console.WriteLine(steps);
+// Find lowest they all divide into...?
+var answer = lowestCommonMultipleInArray(smallestSteps.ToArray());
+
+Console.WriteLine(answer);
+
+return;
+
+// Thanks maths.
+long lowestCommonMultipleInArray(IEnumerable<long> numbers)
+{
+    return numbers.Aggregate(lowestCommonMultiple);
+}
+
+long lowestCommonMultiple(long a, long b)
+{
+    return Math.Abs(a * b) / greatestCommonDivider(a, b);
+}
+
+long greatestCommonDivider(long a, long b)
+{
+    return b == 0 ? a : greatestCommonDivider(b, a % b);
+}
