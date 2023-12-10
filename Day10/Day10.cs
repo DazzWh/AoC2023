16a16cc3351877aa87744fc300a0e3eebdff239d
DazzWh <darrellwhittall@gmail.com>
2023-12-10 11:22:45 +0000

    D10P1

---
+++ Day10/Day10.cs
@@ -0,0 +1,74 @@
+﻿namespace Day10;
+
+public static class Day10
+{
+    public static void Run()
+    {
+        var grid = File.ReadAllLines("Input.txt");
+        var tileMap = ParseMap(grid).ToArray();
+
+        // Find start tile.
+        var startTile = GetStartTile(tileMap);
+        
+        // Get the tiles around the start tile that connect to start tile.
+        var tilesConnectedToStart = startTile
+            .ConnectedPositions()
+            .Where(p => 
+                p.X >= 0 && p.X < tileMap.First().Length &&
+                p.Y >= 0 && p.Y < tileMap.Length)
+            // Start is connected to 4 directions, find the 2 tiles in these directions that claim connection to start. 
+            .Select(p => tileMap.TileAt(p.X, p.Y))
+            .Where(t => t.ConnectedPositions().Any(p => tileMap.TileAt(p) == startTile));
+        
+        // Follow one, taking note of the points as we go
+        var currTile = tilesConnectedToStart.First();
+        var previousTile = startTile; // Store previous tile, to not go backwards when we find connected tiles
+        var loop = new List<Tile>();
+        do
+        {
+            var nextPos = currTile
+                .ConnectedPositions()
+                .First(p => tileMap.TileAt(p) != previousTile);
+                
+            loop.Add(currTile);
+            previousTile = currTile;
+            currTile = tileMap.TileAt(nextPos);
+        } while (currTile.Type != TileType.Start);
+
+        // When we reach the start again find the middle of the loop
+        Console.WriteLine((loop.Count + 1) / 2);
+    }
+
+    private static Tile[][] ParseMap(string[] grid)
+    {
+        var tilemap = new Tile[grid.Length][];
+        for (var y = 0; y < grid.Length; y++)
+        {
+            var newRow = new List<Tile>();
+            for (var x = 0; x < grid[y].Length; x++)
+            {
+                newRow.Add(new Tile(new Position(x, y), grid[y][x].ToTileType()));
+            }
+
+            tilemap[y] = newRow.ToArray();
+        }
+
+        return tilemap;
+    }
+
+    private static Tile GetStartTile(IEnumerable<Tile[]> tileMap)
+    {
+        foreach (var row in tileMap)
+        {
+            foreach (var tile in row)
+            {
+                if (tile.Type == TileType.Start)
+                {
+                    return tile;
+                }
+            }
+        }
+
+        throw new Exception();
+    }
+}
---
+++ Day10/Program.cs
@@ -0,0 +1,151 @@
+﻿Day10.Day10.Run();
+return;
+
+public static class Extenstions
+{
+    public static TileType ToTileType(this char c)
+    {
+        return c switch
+        {
+            '|' => TileType.Vertical,
+            '-' => TileType.Horizontal,
+            'L' => TileType.NorthEast,
+            'J' => TileType.NorthWest,
+            '7' => TileType.SouthWest,
+            'F' => TileType.SouthEast,
+            '.' => TileType.Gound,
+            'S' => TileType.Start,
+            _ => throw new Exception()
+        };
+    }
+
+    public static Tile TileAt(this Tile[][] map, int x, int y)
+    {
+        return map[y][x];
+    }
+    
+    public static Tile TileAt(this Tile[][] map, Position p)
+    {
+        return map[p.Y][p.X];
+    }
+}
+
+public enum TileType
+{
+    Vertical,
+    Horizontal,
+    NorthEast,
+    NorthWest,
+    SouthWest,
+    SouthEast,
+    Gound,
+    Start
+}
+
+static class Direction
+{
+    public static Position North = new (0, -1);
+    public static Position South = new (0,1);
+    public static Position East = new (1, 0);
+    public static Position West = new (-1,0);
+}
+
+public class Position(int x, int y)
+{
+    public int X = x;
+    public int Y = y;
+
+    public override string ToString()
+    {
+        return $"({X},{Y})";
+    }
+
+    public Position Copy()
+    {
+        return new Position(X, Y);
+    }
+    
+    public override bool Equals(object? obj)
+    {
+        var other = obj as Position;
+
+        if (other == null)
+        {
+            return false;
+        }
+
+        return other.X == X && other.Y == Y;
+    }
+
+    protected bool Equals(Position other)
+    {
+        return X == other.X && Y == other.Y;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y);
+    }
+}
+
+public class Tile(Position position, TileType type)
+{
+    public Position Pos = position;
+    public TileType Type = type;
+
+    public List<Position> ConnectedPositions()
+    {
+        var positions = new List<Position>();
+        
+        switch (type)
+        {
+            case TileType.Vertical:
+                positions.Add(Direction.North);
+                positions.Add(Direction.South);
+                break;
+            case TileType.Horizontal:
+                positions.Add(Direction.East);
+                positions.Add(Direction.West);
+                break;
+            case TileType.NorthEast:
+                positions.Add(Direction.North);
+                positions.Add(Direction.East);
+                break;
+            case TileType.NorthWest:
+                positions.Add(Direction.North);
+                positions.Add(Direction.West);
+                break;
+            case TileType.SouthWest:
+                positions.Add(Direction.South);
+                positions.Add(Direction.West);
+                break;
+            case TileType.SouthEast:
+                positions.Add(Direction.South);
+                positions.Add(Direction.East);
+                break;
+            case TileType.Start:
+                positions.Add(Direction.North);
+                positions.Add(Direction.East);
+                positions.Add(Direction.South);
+                positions.Add(Direction.West);
+                break;
+            case TileType.Gound:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(type), type, null);
+        }
+
+        return positions.Select(p => new Position(p.X + Pos.X, p.Y + Pos.Y)).ToList();
+    }
+
+    public override string ToString()
+    {
+        return $"({Type} at {Pos})";
+    }
+
+    protected bool Equals(Tile other)
+    {
+        return Equals(other.Pos, Pos);
+    }
+}
+
